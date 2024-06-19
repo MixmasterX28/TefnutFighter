7fe9c199eaef14f101acb4dd89bb9448ede443a0
MixmasterX28 <144898918+MixmasterX28@users.noreply.github.com>
2024-06-19 12:53:59 +0200

    import scripts

---
+++ TefnutFighter/Assets/Scripts Jamal/PlayerHealth.cs
@@ -0,0 +1,41 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerHealth : MonoBehaviour
+{
+    [SerializeField] private float maxHealth = 100;
+    
+    [SerializeField]private float currentHealth;
+
+    public Healthbar healthbar;
+
+   // void Awake()
+   // {
+   // }
+
+    public void InitHealthBar()
+    {
+        currentHealth = maxHealth;
+        healthbar.SetHealth(maxHealth);
+
+    }
+
+    public void TakeDamage(float damage)
+    {
+        currentHealth -= damage;
+
+        healthbar.SetHealth(currentHealth);
+
+        if (currentHealth <= 0)
+        {
+            //Die();//
+        }
+    }
+
+    private void Die()
+    {
+        Debug.Log(name + " is dead.");
+        gameObject.SetActive(false);
+    }
+}
