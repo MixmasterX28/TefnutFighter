39a02cbbdfa7fc626c0f404c732096300e23d7e9
MixmasterX28 <144898918+MixmasterX28@users.noreply.github.com>
2024-06-26 10:19:04 +0200

    Pillar add

--- TefnutFighter/Assets/Scripts Jamal/Healthbar.cs
+++ TefnutFighter/Assets/Scripts Jamal/Healthbar.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 using UnityEngine.UI;
 
 public class Healthbar : MonoBehaviour
@@ -11,4 +12,5 @@ public class Healthbar : MonoBehaviour
     {
         slider.value = health;
     }
+
 }
